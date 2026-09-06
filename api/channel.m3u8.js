2e3f11ce15de57597946fe32e6fedc4b6d108820
alifadhilalhassany <168203770+alifadhilalhassany@users.noreply.github.com>
2026-09-06 10:36:44 +0300

    Update channel.m3u8.js

--- api/channel.m3u8.js
+++ api/channel.m3u8.js
@@ -1,10 +1,10 @@
 export default async function handler(req, res) {
-  // استقبال رقم القناة، وإذا ماكو رقم يعتبرها القناة الافتراضية 1502
+  // جلب رقم القناة، الافتراضي هو 1502
   const channelId = req.query.id || "1502";
   const targetApiUrl = `https://def.yacinelive.com/api/channel/${channelId}`;
 
   try {
-    // 1. طلب الرابط الجديد من الـ API مع هيدرات تمنع الحظر
+    // 1. طلب الرابط المحدث من API ياسين تيفي مع هيدرات تخطي الحظر
     const apiRes = await fetch(targetApiUrl, {
       headers: {
         "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 12; M2101K7AG Build/SKQ1.210908.001)",
@@ -20,11 +20,11 @@ export default async function handler(req, res) {
 
     const jsonData = await apiRes.json();
 
-    // 2. التأكد من وجود رابط البث الأصلي
+    // 2. التحقق من وجود رابط البث الأصلي
     if (jsonData && jsonData.data && jsonData.data.length > 0 && jsonData.data[0].url) {
       const originalStreamUrl = jsonData.data[0].url;
 
-      // 3. إعادة توجيه (Redirect) مباشرة للـ ExoPlayer على الرابط الطويل الصالح حالياً
+      // 3. إعادة توجيه (Redirect) حية ودائمية للرابط المؤقت الجديد
       res.setHeader('Access-Control-Allow-Origin', '*');
       return res.redirect(302, originalStreamUrl);
     } else {
